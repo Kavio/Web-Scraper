93c0dc1ca26389b66e218a891e6d9659980a34c2
Kavoi Mutisya <kmutisya117@gmail.com>
2023-06-16 14:19:02 -0400

    Now requests all available pages from truecar.com instead of 1

--- webscraper.py
+++ webscraper.py
@@ -1,50 +1,64 @@
 from bs4 import BeautifulSoup
 import requests
+import pandas as pd
 
 cars = []
-page = 1
 
-while(page > )
-true_car = requests.get(f"https://www.truecar.com/used-cars-for-sale/listings/?page=1").text
+true_car = requests.get("https://www.truecar.com/used-cars-for-sale/listings/?page=1").text
 
 soup = BeautifulSoup(true_car, 'lxml')
 
-#print(soup.find_all(''))
-#print(soup.contents)
-
-test = soup.find_all('div', {'data-test' : 'cardContent'})
-
-links = soup.find_all('a', {'data-test' : 'cardContent'})
-print(links)
+def get_total_pages(soup):
 
 
-#print(test[3].find('div', {}).get_text())
-for car in range(len(test)):
-    name = test[car].find('span', {'class':'truncate'}).get_text()
-    year = test[car].find('span', {'class' : 'vehicle-card-year text-xs'}).get_text()
-    price = test[car].find('div', {'data-test' : 'vehicleCardPricingBlockPrice'}).get_text()
-    link = test[car].find('a').get('href')
-    cars.append(name)
+    new_page = soup.find_all('li', {'data-test' : 'paginationDirectionalItem'})
+    print(len(new_page))
+    next_page = new_page[1].find('a').get('href')
 
-    #print(year, name, price, link)
-#print(len(test))
+    total_page_numbers = ""
 
-new_page = soup.find_all('li', {'data-test' : 'paginationDirectionalItem'})
-print(len(new_page))
-next_page = new_page[1].find('a').get('href')
+    for char in next_page:
+        if char.isdigit():
+            total_page_numbers = total_page_numbers + char
 
-total_page_numbers = ""
+    total_page_numbers = int(total_page_numbers) 
 
-for char in next_page:
-    if char.isdigit():
-        total_page_numbers = total_page_numbers + char
-print(total_page_numbers)
+    return total_page_numbers
 
-total_page_numbers = int(total_page_numbers) 
+total_page_numbers = range(int(get_total_pages(soup) / 33))
 
-print(total_page_numbers)
+for page_number in total_page_numbers:
 
-for x in range(int(total_page_numbers / 3)):
-    true_car = requests.get(f"https://www.truecar.com/used-cars-for-sale/listings/?page={x}")
+    true_car = requests.get(f"https://www.truecar.com/used-cars-for-sale/listings/?page={page_number}").text
 
-print(len(cars))
+    soup = BeautifulSoup(true_car, 'lxml')
+
+    test = soup.find_all('div', {'data-test' : 'cardContent'})
+
+    links = soup.find_all('a', {'data-test' : 'cardContent'})
+    #print(links)
+
+    #print(test[3].find('div', {}).get_text())
+    for car in range(len(test)):
+        name = test[car].find('span', {'class':'truncate'}).get_text()
+        year = test[car].find('span', {'class' : 'vehicle-card-year text-xs'}).get_text()
+        price = test[car].find('div', {'data-test' : 'vehicleCardPricingBlockPrice'}).get_text()
+        if price.count('$') > 1:
+            
+            price = price[price.index('$',1):]
+
+        link = test[car].find('a').get('href')
+        car = {
+            "Name": name,
+            "Year": year,
+            "Price": price,
+            "Link": link
+        }
+        cars.append(car)
+        
+df = pd.DataFrame(cars)
+
+print(df)
+        #print(year, name, price, link)
+    #print(len(test))
+df.to_excel('test.xlsx', index=False)
